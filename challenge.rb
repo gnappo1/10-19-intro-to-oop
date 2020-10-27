bbfc84832ff05b449b95bf5303d03d8fe30db015
hansenjl <jenn.leigh.hansen@gmail.com>
2020-10-27 09:01:27 -0700

    finish study group

--- challenge.rb
+++ challenge.rb
@@ -7,3 +7,48 @@ require 'pry'
 # Create a class method to find all of the people from a certain hometown
 
 
+class Person  #Factory for making people
+
+    #macro - code that writes code for us 
+    attr_accessor :name, :age, :height
+    attr_reader :hometown
+
+    def self.all 
+    end
+
+    def initialize(name, city) 
+        @name = name
+        @hometown = city
+    end
+
+    def change_hometown
+        @hometown = "nowhere"
+    end
+
+     #attr_writer :name 
+    # def name=(what_we_want_to_set_the_name_as)   #instance method  # writer/setter  
+    #     @name  = what_we_want_to_set_the_name_as  #instance variable 
+    # end
+
+    #attr_reader :name 
+    # def name #reader or getter method
+    #     @name 
+    # end
+
+    # def age=(age)
+    #     @age = age
+    # end
+
+    # def age
+    #     @age 
+    # end
+
+    def say_hello
+        first_name = "Jennifer"
+        puts "Hi, my name is #{self.name} or #{first_name}"
+    end
+
+
+end
+
+binding.pry
--- test.rb
+++ test.rb
@@ -1,1 +1,13 @@
 # What are some ways we can represent a dog with code?
+dog = {name: "Toby", age: 10}  # hash
+dog_array = ["Toby", 10] # array 
+dog_name = "Toby" # string
+dog_age = 10 # integer
+is_a_dog? = true #boolean
+
+# Everything is ruby is either an object or a method
+# AND all methods are called upon objects
+
+"Toby".split("")
+
+dog_age + 1 
